3267976e1960e4179919129d8e0da18a9b70e83c
Joshua Tauberer <jt@occams.info>
2015-09-23 16:40:43 +0000

    add a ?blur=1 and ?quality=low filters to image files

--- dbstorage/views.py
+++ dbstorage/views.py
@@ -18,9 +18,9 @@ def get_file_content_view(request, path):
 	file_content = sf.get_blob()
 	mime_type = sf.mime_type or "application/octet-stream"
 
-	if "size" in request.GET:
+	if "size" in request.GET or "blur" in request.GET or "quality" in request.GET:
 		try:
-			file_content = transform_image(file_content, request.GET['size'])
+			file_content, mime_type = transform_image(file_content, request.GET)
 		except Exception as e:
 			return HttpResponse(str(e), status=500)
 
@@ -28,22 +28,33 @@ def get_file_content_view(request, path):
 	response['Content-Disposition'] = 'inline; filename=' + os.path.basename(path)
 	return response
 
-def transform_image(file_content, size):
+def transform_image(file_content, options):
 	import io
-	from PIL import Image
+	from PIL import Image, ImageFilter
 
-	try:
-		# Get the desired width from a small range of options.
-		# 'tb' (thumbnail) is intended to be at least as good for og:image URLs,
-		# which for twitter summary cards must be at least 120px.
-		width = { 'tb': 320, 'xs': 768, 'sm': 1024, 'md': 1100, 'lg': 1400 }[size]
-	except KeyError:
-		return file_content
+	# Load the image.
+	im = Image.open(io.BytesIO(file_content))
+
+	# Blur, if specified.
+	if options.get('blur'):
+		im = im.filter(ImageFilter.GaussianBlur(radius=3))
+
+	# Resize, if specified.
+
+	# Get the desired width from a small range of options.
+	# 'tb' (thumbnail) is intended to be at least as good for og:image URLs,
+	# which for twitter summary cards must be at least 120px.
+	width = { 'tb': 320, 'xs': 768, 'sm': 1024, 'md': 1100, 'lg': 1400 }.get(options.get('size'))
 
 	# Resize the image and output at low quality jpeg to
 	# reduce bandwidth usage.
-	im = Image.open(io.BytesIO(file_content))
-	im.thumbnail((width, width), Image.ANTIALIAS)
+	if width:
+		im.thumbnail((width, width), Image.ANTIALIAS)
+
+	# Re-serialize to JPEG.
 	buf = io.BytesIO()
-	im.save(buf, "JPEG", quality=25, optimize=True, progressive=True)
-	return buf.getvalue()
+	if options.get('quality') == 'low':
+		im.save(buf, "JPEG", quality=33, optimize=True, progressive=True)
+	else:
+		im.save(buf, "JPEG")
+	return (buf.getvalue(), "image/jpeg")
