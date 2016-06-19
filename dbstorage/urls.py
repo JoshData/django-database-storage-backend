ed0cdaccfc2478a1ecdb5573ed9a26ba9ef27909
Joshua Tauberer <jt@occams.info>
2016-06-19 12:52:22 -0400

    fix a Django warning

--- dbstorage/urls.py
+++ dbstorage/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 import dbstorage.views
 
 urlpatterns = [
-	url(r'^/(.*)', dbstorage.views.get_file_content_view, name='get_file_content_view'),
+	url(r'^(.*)', dbstorage.views.get_file_content_view, name='get_file_content_view'),
 ]
